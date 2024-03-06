29a6f9c9e7ffb2c08afc42bad855eb65e43965eb
Larissa Saraiva <larssaraiva@gmail.com>
2024-03-05 21:32:32 -0300

    Projeto 1

---
+++ projeto1.rb
@@ -0,0 +1,8 @@
+puts "Olá!"
+print "Digite seu nome: "
+nome = gets.chomp
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+print "Digite sua idade: "
+id = gets.chomp.to_i
+puts "Usuário(a): #{nome} #{sobrenome}, #{id} anos."
